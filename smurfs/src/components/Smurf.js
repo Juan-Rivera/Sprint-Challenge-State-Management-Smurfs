0a4911f3e968866d464f4f3372cb5793c4877348
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-15 12:35:07 -0400

    Basically mapped the smurf context inside of SmurfDisplay and returns a component that is built out to display the name age and height

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Smurf = props => {
+	return (
+		<div className="smurf">
+			<h1>Name: {props.smurf.name}</h1>
+            <p>Age: {props.smurf.age}</p>
+            <p>Height: {props.smurf.height} </p>
+		</div>
+	);
+};
+export default Smurf;
--- smurfs/src/components/SmurfDisplay.js
+++ smurfs/src/components/SmurfDisplay.js
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
 import { SmurfContext } from '../contexts/SmurfContext';
 
+import Smurf from './Smurf';
+
 const SmurfDisplay = () => {
     const { smurf } = useContext(SmurfContext)
-    console.log(smurf)
+    
    return(
-       <div>
-           
+       <div className='smurfDisplay'>
+           {smurf.map(smurf => {
+              return <Smurf key={smurf.id} smurf={smurf}/>
+           })}
        </div>
    ) 
 }
